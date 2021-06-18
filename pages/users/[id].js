6ad52ee343008cc91d341fcfd3a39d5ae2c6fe90
Vadim Barkovski <VBarkovski@gomel.iba.by>
2021-06-18 14:05:01 +0300

    added userId component

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,7 +1,9 @@
 import {useRouter} from "next/router";
 import {useEffect} from "react";
 import usersList from "../../store/usersList";
-import {observer} from "mobx-react-lite"
+import {observer} from "mobx-react-lite";
+import styles from "../../styles/userid.module.css";
+import Header from "../../components/header";
 
 const UserId = observer(() => {
     const {query} = useRouter();
@@ -11,8 +13,36 @@ const UserId = observer(() => {
 
     return (
       <div>
-        User with ID {query.id}
-        <p>{usersList.user.name}</p>
+        <Header/>
+        <div className={styles.userid_body}>
+          <p>{usersList.user.id}</p>
+          <div className={styles.userid_items}>
+            <img src={usersList.user.fullImage} alt={usersList.user.name}/>
+            <p>{`${usersList.user.name} ${usersList.user.lastName}`}</p>
+            <p>{usersList.user.login}</p>
+            <p>{usersList.user.email}</p>
+            <p>{usersList.user.phone}</p>
+            <div className={styles.userid_buttons_body}>
+              <button
+                className={styles.userid_button}
+                type={"button"}
+                onClick={() => usersList.ratingHandler(false)}
+                disabled={usersList.user.rating === 0}
+              >
+                -
+              </button>
+              <p>{usersList.user.rating}</p>
+              <button
+                className={styles.userid_button}
+                type={"button"}
+                onClick={() => usersList.ratingHandler(true)}
+                disabled={usersList.user.rating === 5}
+              >
+                +
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
