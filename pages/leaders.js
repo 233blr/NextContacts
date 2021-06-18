7175fa069ff91d5edbf0313934431e18a2ce26d0
Vadim Barkovski <VBarkovski@gomel.iba.by>
2021-06-18 15:25:33 +0300

    fixed leaders component

--- pages/leaders.js
+++ pages/leaders.js
@@ -3,6 +3,7 @@ import Header from "../components/header";
 import styles from "../styles/leaders.module.css";
 import usersList from "../store/usersList";
 import Leader from "../components/leader";
+import Anchor from "../components/anchor";
 
 const Leaders = () => {
   return (
@@ -10,7 +11,12 @@ const Leaders = () => {
       <Header/>
       <h1 className={styles.heading}>Leaders</h1>
       <div>
-        {usersList.sortLeaders().map(item => <Leader key={item.id} leader={item}/>)}
+        {usersList.sortLeaders().length ?
+          usersList.sortLeaders().map(item => <Leader key={item.id} leader={item}/>) :
+          <div className={styles.leaders_alert}>
+            <p>No Leaders 🧐!!!</p>
+            <p><Anchor text={'go to Users page'} href={'./users'}/></p>
+          </div>}
       </div>
     </div>
   );
